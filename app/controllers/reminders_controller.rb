6a37b5aa04fecd72d6bf9702fc16bd4f95e8fe79
Kate <kbalducci@hotmail.com>
2015-02-16 21:19:52 -0600

    successfully posted reminder

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,6 +1,43 @@
 class RemindersController < ApplicationController
+  before_action :authenticate_user!
 
+  def new
+    set_user
+    set_new_reminder
+  end
+
+  def create
+      @reminder = Reminder.new(reminder_params)
+      user = current_user
+      @reminder.user_id = user.id
+      respond_to do |format|
+        if @reminder.save
+          format.html { redirect_to root_path, notice: 'Reminder was successfully created.' }
+          format.json { render :show, status: :created, location: @reminder }
+
+        else
+          format.html { render :new }
+          format.json { render json: @reminder.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+
+  private
+    def set_reminder
+      @reminder = Reminder.find(params[:id])
+    end
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+
+    def set_new_reminder
+      @new_reminder = Reminder.new
+    end
     def reminder_params
-      params.require(:user).permit(:user_id, :title, :description, :priority)
+      params.require(:reminder).permit(:user_id, :title, :description, :priority)
+    end
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :user_name)
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,20 @@ def new
   @reminder = Reminder.new
 end
 
+def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render "welcome/index" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:first_name, :last_name, :user_name)
